26a42ff06103141d4f9ae950776d9a7796eb25a9
aonomike <aonomike@gmail.com>
2019-03-17 18:08:07 +0300

    Add hound
    - Add hound dependency and configuration
    - write first test

--- config/test.exs
+++ config/test.exs
@@ -4,15 +4,26 @@ use Mix.Config
 # you can enable the server option below.
 config :mango_two, MangoTwoWeb.Endpoint,
   http: [port: 4002],
-  server: false
+  server: true
 
 # Print only warnings and errors during test
 config :logger, level: :warn
 
 # Configure your database
 config :mango_two, MangoTwo.Repo,
-  username: "postgres",
-  password: "postgres",
+  username: "globalprograms",
+  password: "",
   database: "mango_two_test",
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
+
+#configure hound to use chrome driver 
+config :hound,
+driver: "chrome_driver",
+browser: "chrome_headless",
+additional_capabilities: %{
+  chromeOptions: %{ "args" => [
+    "--headless"
+  ]}
+}
+
